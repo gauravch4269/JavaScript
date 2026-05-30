20e0735b730489661d404e9cefc55e909649ad81
gauravch4269 <gaurav900g@gmail.com>
2026-05-30 13:45:29 +0530

    js

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // traet all JS code as newer version
+// copy notes
+
+console.log(typeof "guarav")
+console.log(typeof 12)
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object on run
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "hello@gmail.com"
+// var accountPassword = "12345" Not preffered
+accountCity = "Lucknow" 
+
+// accountId=24452  can't change the value
+accountEmail="world@gmail.com"
+accountPassword="6789"
+ 
+console.log(accountId)
+
