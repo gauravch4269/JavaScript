9c2916c34ec2d01eaa32580399a8252b07000119
gauravch4269 <gaurav900g@gmail.com>
2026-05-31 13:04:45 +0530

    conversions

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,17 @@
+let score = 33
+let change = Number(score)
+console.log(typeof change)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+ 
+let loggedIn = 1
+
+let booleanIsLoggedIn = Boolean(loggedIn) // give true
+// 1 => true, 0 => false
+// "" => false
+// "abc" => true
+
+let a = 33
+let b = String(a)
