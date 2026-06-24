7f3f8e96246a61be539446c3ebbbba79e3a71909
gauravch4269 <gaurav900g@gmail.com>
2026-06-25 00:21:07 +0530

    DOM

---
+++ 05_iterations/nine.js
@@ -0,0 +1,8 @@
+// reduce() - reduce the array into a single value
+const myNums = [1,2,3]
+const myTotal = myNums.reduce(function(acc,currval) {
+    console.log(`acc:${acc} and currval: ${currval}`);
+    return acc+currval
+    
+} , 0)
+console.log
--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -4,4 +4,8 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 // }))
 
 //Chaining - mean do teen method ek sath laga sakte hai
+//const newNums = myNumbers.map().map() - first ka result second me jayega
+const newNums = myNumbers
+                .map((num) => num*10)
+                .map((num) => num+1) // upar wale ka result isme aayega  
 
